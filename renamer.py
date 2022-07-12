f59c828814a647e7121d6db3898fe0889d269fde
Claus Juhl Knudsen <cjk@aarhus.dk>
2022-07-12 14:49:52 +0200

    Convert to poetry

--- renamer.py
+++ renamer.py
@@ -3,8 +3,7 @@ import sys
 import os
 from pathlib import Path
 
-
-if __name__ == "__main__":
+def main():
     if sys.argv[1] == "--help":
         print(
             "Run with python renamer.py db_file_path puid new_extension_without_period_sign."
@@ -20,17 +19,28 @@ if __name__ == "__main__":
         connection = sqlite3.connect(db_path)
         cursor = connection.cursor()
 
-        rows = cursor.execute(DB_QUERY_GET_FILES)
-
+        cursor.execute(DB_QUERY_GET_FILES)
+        rows = cursor.fetchall()
         for row in rows:
             rel_path = row[0].replace("\\", "/")
             absolute_path: Path = ROOTPATH / rel_path
             try:
                 absolute_path.rename(str(absolute_path) + "." + new_suffix)
+            except FileNotFoundError:
+                print(f"Could not find file: {absolute_path}", flush=True)
+            except Exception as e:
+                print(f"Unable to rename {absolute_path}: {e}", flush=True)
+            else:
                 new_rel_path = row[0] + "." + new_suffix
                 update_query = f"UPDATE Files SET relative_path = '{new_rel_path}', warning = 'Corrected extension mismatch' WHERE Files.uuid = '{row[1]}'"
-                cursor.execute(update_query)
-                connection.commit()
-            except FileNotFoundError:
-                print("Could not find the file with path: " + str(absolute_path))
+                try:
+                    cursor.execute(update_query)
+                except Exception as e:
+                    print(f"Unable to update db for {new_rel_path}: {e}", flush=True)
+
+        connection.commit()
         connection.close()
+
+
+if __name__ == "__main__":
+    main()
